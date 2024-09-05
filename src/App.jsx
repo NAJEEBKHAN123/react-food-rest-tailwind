785554c030ed06ef40e5266fa43615e00d49bb19
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-09-05 21:48:41 +0500

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+
+function App() {
+  return (
+    <>
+     <Navbar/>
+     <Home/>
+    </>
+  )
+}
+
+export default App
